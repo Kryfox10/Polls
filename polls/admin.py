a8393d994278e8e1db60ac1c6cdc3542a4a5d00e
Mon <kryfox10@gmail.com>
2013-09-20 10:39:14 +0800

    Almost done with Chapter 2.

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from polls.models import Poll
-from polls.models import Choice
+from polls.models import Choice, Poll
 
 #dividers
 #class PollAdmin(admin.ModelAdmin):
@@ -10,11 +9,26 @@ from polls.models import Choice
 #    ]
 
 # collapse class
+#class PollAdmin(admin.ModelAdmin):
+#    fieldsets = [
+#        (None,               {'fields': ['question']}),
+#        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+#    ]
+#
+#admin.site.register(Poll, PollAdmin)
+#admin.site.register(Choice)
+#
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
 
 admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
