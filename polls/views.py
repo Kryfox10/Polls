6e78f4018d3df0e5edc39104a4d5b491c2eb8686
Mon <kryfox10@gmail.com>
2013-10-08 09:31:15 +0800

    Implemented Http404 Error for details view.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.http import Http404
 
 #ommited code below for render()
 #from django.template import RequestContext, loader
@@ -22,7 +23,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-	return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
 	return HttpResponse("You're looking at the results of poll %s." % poll_id)
