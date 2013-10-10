0122c08e5e378467d72d8a7a94d40b4b1d980c70
Mon <kryfox10@gmail.com>
2013-10-11 00:27:34 +0800

    Modified vote() function

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,12 @@
 # Create your views here.
-from django.http import HttpResponse
-from django.http import Http404
+##from django.http import HttpResponse
+##from django.http import Http404
 
 #ommited code below for render()
 #from django.template import RequestContext, loader
-from django.shortcuts import render
+##from django.shortcuts import render
 
-from polls.models import Poll
+##from polls.models import Poll
 
 #ommited code below to use render()
 # def index(request):
@@ -16,6 +16,13 @@ from polls.models import Poll
 #         'latest_poll_list': latest_poll_list,
 #     })
 #     return HttpResponse(template.render(context))
+
+#Chapter 4 codes
+
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.core.urlresolvers import reverse
+from polls.models import Choice, Poll
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
@@ -33,4 +40,19 @@ def results(request, poll_id):
 	return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 def vote(request, poll_id):
-	return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form.
+        return render(request, 'polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
